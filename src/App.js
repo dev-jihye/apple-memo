1cde8b3942a658cc27d506812de0d42563d15f37
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-05-25 17:11:42 +0900

    ✨ add memo 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
+import { useState } from 'react';
 import MemoItem from './components/MemoItem';
-import memos from './mock.json';
 import './style.css';
 
 function App() {
+  const [memos, setMemos] = useState([]);
+  const [selectedMemo, setSelectedMemo] = useState(null);
+
+  const onAddMemo = () => {
+    if (memos.length >= 1 && !memos[0].content) {
+      return;
+    }
+
+    const newMemo = {
+      id: Date.now(),
+      content: '',
+    };
+
+    setMemos([newMemo, ...memos]);
+    setSelectedMemo(newMemo);
+  };
 
   return (
     <div className="App">
       <div className="memoContainer">
         <div className="nav">
-          <button className="btn">메모 작성하기</button>
+          <button className="btn" onClick={() => onAddMemo()}>
+            메모 작성하기
+          </button>
         </div>
         <section className="memoSection">
           <div className="listArea">
